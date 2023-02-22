74685e3952c9b559156e6a056d3a17b008c4a9ad
jcasare <asarejerry16@gmail.com>
2023-02-22 21:37:34 +0000

    app.js setup

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 //jshint esversion:6
+const express = require("express");
+const app = express();
+const port = 3000 || process.env.PORT;
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
