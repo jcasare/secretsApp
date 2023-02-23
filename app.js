9e5365ab22c86d2d0ad884c4403bd64758a30028
jcasare <asarejerry16@gmail.com>
2023-02-23 13:38:43 +0000

    project skeleton setup

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 const express = require("express");
 const app = express();
 const port = 3000 || process.env.PORT;
+const ejs = require("ejs");
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+app.get("/register", (req, res) => {
+  res.render("register");
+});
 
 app.listen(port, () => {
   console.log(`server is listening on port ${port}`);
